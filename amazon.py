dee9ad5d4065aa7fca31d11542cf9516064f86b2
victoriamkr <victoria.mkr@gmail.com>
2020-01-28 11:34:46 +0400

    - homework, find element

---
+++ amazon.py
@@ -0,0 +1,23 @@
+import selenium
+from selenium.webdriver.common.keys import Keys
+
+browser = selenium.webdriver.Safari()
+
+browser.get('http://www.amazon.com')
+assert 'Amazon' in browser.title
+
+browser.implicitly_wait(5)
+browser.maximize_window()
+
+elem = browser.find_element_by_id('twotabsearchtextbox')  # Find the search box
+elem.send_keys('testing' + Keys.RETURN)
+
+searchresults = browser.find_elements_by_xpath('//div[@class="a-section a-spacing-none"]/h2/a/span')
+
+for el in searchresults:
+    print el.text
+
+
+#browser.quit()
+
+#searchresults = browser.find_elements_by_xpath('//div[@class="a-section a-spacing-none"]/h2/a/span')
